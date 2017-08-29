1ac1ae34d1c052dd3d9f67bc659b63fabadec250
Mitchell F Chan <mitchellfchan@gmail.com>
2017-08-29 07:34:56 -0400

    Final Deployment
    as found at 0x88AE96845e157558ef59e9Ff90E766E22E480390

--- IKB.sol
+++ IKB.sol
@@ -1,4 +1,4 @@
-//      KLEIN v. 5.06
+//      IKB TOKEN
 //      By Mitchell F. Chan
 
 
@@ -68,15 +68,15 @@ contract Klein is ERC20, owned, SafeMath {
     
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
-
+    mapping (address => mapping (address => mapping (uint256 => bool))) specificAllowed;
+    
                                                                     // The Swarm address of the artwork is saved here for reference and posterity
     string public constant zonesSwarmAddress = "0a52f265d8d60a89de41a65069fa472ac3b130c269b4788811220b6546784920";
-    address public constant theRiver = 0xF61FB82093Cd22Ca53dBab8d8d7ad25AD97c6a6E;                      // ROPSTEN REVIVAL address
+    address public constant theRiver = 0x8aDE9bCdA847852DE70badA69BBc9358C1c7B747;                      // ROPSTEN REVIVAL address
     string public constant name = "Digital Zone of Immaterial Pictorial Sensibility";
     string public constant symbol = "IKB";
     uint256 public constant decimals = 0;
     uint256 public maxSupplyPossible;
-    uint256 public saleStartTime;                                      // will be initialized to 8:30PM EST August 30, 2017
     uint256 public initialPrice = 10**17;                              // should equal 0.1 ETH
     uint256 public currentSeries;    
     uint256 public issuedToDate;
@@ -102,22 +102,23 @@ contract Klein is ERC20, owned, SafeMath {
     
     event UpdateRecord(uint indexed IKBedition, address holderAddress, uint256 price, bool burned);
     event SeriesCreated(uint indexed seriesNum);
+    event SpecificApproval(address indexed owner, address indexed spender, uint256 indexed edition);
     
-    function Klein(uint256 _saleStartTime) {
-        saleStartTime = _saleStartTime;
+    function Klein() {
         currentSeries = 0;
         series[0] = IKBSeries(initialPrice, 31);                    // the first series has unique values...
-        
+    
         for(uint256 i = 1; i < series.length; i++){                    // ...while the next 7 can be defined in a for loop
             series[i] = IKBSeries(series[i-1].price*2, 10);
-        }            
+        }     
+        
         maxSupplyPossible = 101;
     }
     
     function() payable {
         buy();
     }
-    
+
     function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
     }
@@ -127,6 +128,12 @@ contract Klein is ERC20, owned, SafeMath {
         Approval(msg.sender, _spender, _value);
         return true;
     }
+    
+    function specificApprove(address _spender, uint256 _edition) returns (bool success) {
+        specificAllowed[msg.sender][_spender][_edition] = true;
+        SpecificApproval(msg.sender, _spender, _edition);
+        return true;
+    }
 
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
       return allowed[_owner][_spender];
@@ -134,11 +141,10 @@ contract Klein is ERC20, owned, SafeMath {
 
     // NEW: I thought it was more in keeping with what totalSupply() is supposed to be about to return how many tokens are currently in circulation
     function totalSupply() constant returns (uint _currentSupply) {
-      return issuedToDate;
+      return (issuedToDate - burnedToDate);
     }
 
     function issueNewSeries() onlyOwner returns (bool success){
-        require(now >= saleStartTime);                           //you can only issue a new series if the sale has started
         require(balances[this] <= 0);                            //can only issue a new series if you've sold all the old ones
         require(currentSeries < 7);
         
@@ -153,20 +159,20 @@ contract Klein is ERC20, owned, SafeMath {
         SeriesCreated(currentSeries);
         return true;
     }
-
+    
     function buy() payable returns (bool success){
         require(balances[this] > 0);
         require(msg.value >= series[currentSeries].price);
-        require(now >= saleStartTime);
         uint256 amount = msg.value / series[currentSeries].price;      // calculates the number of tokens the sender will buy
-
-        if (msg.value % series[currentSeries].price > 0) assert(returnChange());
+        uint256 receivable = msg.value;
         if (balances[this] < amount) {                              // this section handles what happens if someone tries to buy more than the currently available supply
-            uint256 receivable = safeMult(balances[this], series[currentSeries].price);
+            receivable = safeMult(balances[this], series[currentSeries].price);
             uint256 returnable = safeSubtract(msg.value, receivable);
             amount = balances[this];
             msg.sender.transfer(returnable);             
         }
+        
+        if (receivable % series[currentSeries].price > 0) assert(returnChange(receivable));
         
         balances[msg.sender] = safeAdd(balances[msg.sender], amount);                             // adds the amount to buyer's balance
         balances[this] = safeSubtract(balances[this], amount);      // subtracts amount from seller's balance
@@ -179,10 +185,9 @@ contract Klein is ERC20, owned, SafeMath {
         
         return true;                                   // ends function and returns
     }
-    
-    //NEW: as per our conversation
-    function returnChange() internal returns (bool success){
-        uint256 change = msg.value % series[currentSeries].price;
+
+    function returnChange(uint256 _receivable) internal returns (bool success){
+        uint256 change = _receivable % series[currentSeries].price;
         msg.sender.transfer(change);
         return true;
     }
@@ -205,46 +210,6 @@ contract Klein is ERC20, owned, SafeMath {
         Transfer(msg.sender, _to, _value);
         return true;
     }
-                                                                    // when this function is called, the caller is transferring only 1 IKB to another account, and specifying exactly which token they would like to transfer.
-    function specificTransfer(address _to, uint _edition) returns (bool success) {
-        require(balances[msg.sender] > 0);
-        require(records[_edition].addr == msg.sender); 
-        balances[msg.sender] = safeSubtract(balances[msg.sender], 1);
-        balances[_to] = safeAdd(balances[_to], 1);
-        records[_edition].addr = _to;                           // update the records so that the record shows this person owns the 
-        
-        Transfer(msg.sender, _to, 1);
-        UpdateRecord(_edition, _to, records[_edition].price, records[_edition].burned);
-        return true;
-    }
-    
-    function specificTransferFrom(address _from, address _to, uint _edition) returns (bool success){
-        require(balances[_from] > 0);
-        require(records[_edition].addr == _from);
-        require(allowed[_from][msg.sender] > 0);
-        balances[_from] = safeSubtract(balances[_from], 1);
-        balances[_to] = safeAdd(balances[_to], 1);
-        allowed[_from][msg.sender] = safeSubtract(allowed[_from][msg.sender], 1);
-        records[_edition].addr = _to;                           // update the records so that the record shows this person owns the 
-        
-        Transfer(msg.sender, _to, 1);
-        UpdateRecord(_edition, _to, records[_edition].price, records[_edition].burned);
-        return true;
-    }
-                                                                    // a quick way to figure out who holds a specific token without querying the whole record. This might actually be redundant.
-    function getTokenHolder(uint searchedRecord) public constant returns(address){
-        return records[searchedRecord].addr;
-    }
-    
-    // function getHolderEditions(address _holder) returns (uint256[] _editions) {
-    //     uint256[] memory editionsOwned;
-    //     for(uint256 k = 0; k < records.length; k++) {
-    //         if(records[k].addr == _holder) {
-    //             editionsOwned.push[k];
-    //         }
-    //     }
-    //     return editionsOwned;
-    // }
     
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         require(balances[_from] >= _value); 
@@ -265,7 +230,49 @@ contract Klein is ERC20, owned, SafeMath {
         balances[_to] = safeAdd(balances[_to], _value); 
         Transfer(_from, _to, _value);
         return true;     
-    }        
+    }   
+                                                                    // when this function is called, the caller is transferring only 1 IKB to another account, and specifying exactly which token they would like to transfer.
+    function specificTransfer(address _to, uint _edition) returns (bool success) {
+        require(balances[msg.sender] > 0);
+        require(records[_edition].addr == msg.sender); 
+        balances[msg.sender] = safeSubtract(balances[msg.sender], 1);
+        balances[_to] = safeAdd(balances[_to], 1);
+        records[_edition].addr = _to;                           // update the records so that the record shows this person owns the 
+        
+        Transfer(msg.sender, _to, 1);
+        UpdateRecord(_edition, _to, records[_edition].price, records[_edition].burned);
+        return true;
+    }
+    
+    function specificTransferFrom(address _from, address _to, uint _edition) returns (bool success){
+        require(balances[_from] > 0);
+        require(records[_edition].addr == _from);
+        require(specificAllowed[_from][msg.sender][_edition]);
+        balances[_from] = safeSubtract(balances[_from], 1);
+        balances[_to] = safeAdd(balances[_to], 1);
+        specificAllowed[_from][msg.sender][_edition] = false;
+        records[_edition].addr = _to;                           // update the records so that the record shows this person owns the 
+        
+        Transfer(msg.sender, _to, 1);
+        UpdateRecord(_edition, _to, records[_edition].price, records[_edition].burned);
+        return true;
+    }
+                                                                    // a quick way to figure out who holds a specific token without querying the whole record. This might actually be redundant.
+    function getTokenHolder(uint searchedRecord) public constant returns(address){
+        return records[searchedRecord].addr;
+    }
+    
+    function getHolderEditions(address _holder) public constant returns (uint256[] _editions) {
+        uint256[] memory editionsOwned = new uint256[](balances[_holder]);
+        uint256 index;
+        for(uint256 k = 0; k < records.length; k++) {
+            if(records[k].addr == _holder) {
+                editionsOwned[index] = k;
+                index++;
+            }
+        }
+        return editionsOwned;
+    }
                                                                     // allows the artist to withdraw ether from the contract
     function redeemEther() onlyOwner returns (bool success) {
         owner.transfer(this.balance);  
